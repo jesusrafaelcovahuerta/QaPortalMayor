0d840c373ea6bc1cbe113695323f9abdbbd6ec48
jisjesuscova <jesuscova@jisparking.com>
2023-09-11 10:28:05 -0300

    cambios

--- resources/js/components/EditContent.vue
+++ resources/js/components/EditContent.vue
@@ -41,6 +41,7 @@
                                         <label for="exampleInputEmail1">Sección - Categoría <h6 class="m-0 text-danger float-right">*</h6></label>
                                         <select class="form-control" id="exampleFormControlSelect1"
                                         v-model="form.category_id"
+                                        @change="getRegionsCommunes"
                                         >
                                             <option :value="null">-Seleccionar-</option>
                                             <option v-for="category_post in category_posts" :key="category_post.category_id" :value="category_post.category_id">{{ category_post.section_title }} - {{ category_post.title }}</option>
@@ -206,7 +207,25 @@
                                         >
                                     </div>
                                 </div>
-
+                                <div class="form-group row" v-if="form.georeferencing_type_id == 1">
+                                    <div class="col-sm-6">
+                                        <label for="exampleInputEmail1">Región <h6 class="m-0 text-danger float-right">*</h6></label>
+                                        <select class="form-control" id="exampleFormControlSelect1"
+                                        v-model="form.region_id" multiple
+                                        @change="getCommunes"
+                                        >
+                                            <option :value="1000">Todas las regiones y comunas</option>
+                                            <option :selected="isSelectedRegion(region_post.region_id)" v-for="region_post in region_posts" :key="region_post.region_id" :value="region_post.region_id">{{ region_post.region }}</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label for="exampleInputEmail1">Comuna</label>
+                                        <select class="form-control" id="exampleFormControlSelect1" v-model="form.commune_id"  multiple>
+                                            <option :value="null" v-if="commune_posts.length == 0">No ha seleccionado una región</option>
+                                            <option :selected="isSelectedCommune(commune_post.commune_id)" v-for="commune_post in commune_posts" :key="commune_post.commune_id" :value="commune_post.commune_id">{{ commune_post.commune }}</option>
+                                        </select>
+                                    </div>
+                                </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6">
                                         <label for="exampleInputEmail1">¿El enlace de compartir de Whatsapp es personalizado? <h6 class="m-0 text-danger float-right">*</h6></label>
@@ -269,6 +288,8 @@
             this.getRols();
             this.storeAudit();
             this.getPost();
+            this.getRegions();
+            this.getCommunes();
             this.getCategoryList();
         },
         data: function() {
@@ -278,6 +299,8 @@
                 color: '#0A2787',
                 loading: false,
                 category_posts: [],
+                region_posts: [],
+                commune_posts: [],
                 noFile: false,
                 noPdf: false,
                 form: {
@@ -301,10 +324,50 @@
                     position: '',
                     whatsapp_type_id: 2,
                     whatsapp_description: '',
+                    region_id: null,
+                    commune_id: null,
+                    georeferencing_type_id: 2,
                 }
             }
         },
         methods: {
+            getRegions() {
+                this.loading = true;
+
+                axios.get('/api/region')
+                .then(response => {
+                    this.region_posts = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            },
+            getCommunes() {
+                this.loading = true;
+
+                this.commune_posts = []
+
+                var region_data = String(this.form.region_id);
+
+                const region_ids = region_data.split(',');
+
+                for (const region_id of region_ids) {
+                    console.log(region_id)
+                    axios.get('/api/commune/' + region_id)
+                        .then(response => {
+                            this.commune_posts = this.commune_posts.concat(response.data.data);
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        })
+                        .finally(() => {
+                            this.loading = false;
+                        });
+                }
+            },
             getRols() {
                 axios.get('/api/user/rol?api_token=' + App.apiToken)
                     .then(response => {
@@ -315,6 +378,35 @@
                         });
 
                     });
+            },
+            async getRegionsCommunes() {
+                try {
+                    const response = await axios.get('/api/category_region/' + this.$route.params.id + '/edit?api_token='+App.apiToken);
+
+                    this.stored_regions = response.data.data;
+
+                    this.loading = false;
+
+                    const selectedRegionIds = this.stored_regions.map(item => item.region_id);
+                    this.form.region_id = selectedRegionIds;
+                } catch (error) {
+                    console.error(error);
+                }
+
+                try {
+                    const response = await axios.get('/api/category_commune/' + this.$route.params.id + '/edit?api_token='+App.apiToken);
+
+                    this.stored_communes = response.data.data;
+
+                    this.loading = false;
+
+                    const selectedCommuneIds = this.stored_communes.map(item => item.commune_id);
+                    this.form.commune_id = selectedCommuneIds;
+
+                    console.log(this.form.commune_id);
+                } catch (error) {
+                    console.error(error);
+                }
             },
             storeAudit() {
                 let formData = new FormData();
@@ -386,6 +478,10 @@
                     }
 
                     this.$set(this.form, 'icon_available_id', this.post.icon_available_id);
+
+                    this.$set(this.form, 'georeferencing_type_id', this.post.georeferencing_type_id);
+                    this.$set(this.form, 'region_id', this.post.region_id);
+                    this.$set(this.form, 'commune_id', this.post.commune_id);
 
                     this.loading = false;
                 } catch (error) {
